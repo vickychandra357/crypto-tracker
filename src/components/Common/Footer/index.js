0a4262f8ef719e0a638227250950f1678699c4f7
tonystark <chandravicky288@gmail.com>
2023-07-15 15:12:15 +0530

    first

---
+++ src/components/Common/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+function Footer(){
+    return (
+        <div className="footer">
+            Hello Footer
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Common/Header/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./styles.css";
+import TemporaryDrawer from "./drawer";
+import Button from "../Botton";
+
+
+function Header(){
+    return (
+        <div className="navbar">
+            <h1 className="logo">CryptoTracker<span style={{color:"var(--blue)"}}>.</span></h1>
+            <div className="links">
+                <a href="/">
+                    <p className="link">Home</p>
+                </a>
+                <a href="/">
+                <p className="link">Compare</p>
+                </a>
+                <a href="/">
+                <p className="link">Watchlist</p>
+                </a>
+                <a href="#">
+                <Button text={"Dashboard"} 
+                outlined={true}
+                onClick={()=>console.log("Btn Clicked")}
+                />
+                </a>
+            </div>
+            <div className="mobile-drawer">
+                <TemporaryDrawer />
+            </div>
+        </div>
+    )
+}
+
+export default Header;
